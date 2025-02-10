6c49e6ef89f4e4fb2c51e598a7b45bcd97b1ae28
Kimi Li <qianbing@hawaii.edu>
2025-02-10 13:22:54 -1000

    finished with E27

--- ziplist.js
+++ ziplist.js
@@ -1,4 +1,11 @@
 "use strict";
+function zipList(list1, list2) {
+    const result = [];
+    for (let i = 0; i < list1.length; i++) {
+        result.push(list1[i], list2[i]);
+    }
+    return result;
+}
 function zipListTheFunctionalWay(list1, list2) {
     return list1.reduce((acc, value, index) => {
         acc.push(value, list2[index]);
@@ -8,3 +15,4 @@ function zipListTheFunctionalWay(list1, list2) {
 const list1 = ['a', 'b', 'c'];
 const list2 = [1, 2, 3];
 console.log(zipListTheFunctionalWay(list1, list2));
+console.log(zipList(list1, list2));
--- ziplist.ts
+++ ziplist.ts
@@ -1,3 +1,11 @@
+function zipList(list1: string[], list2: number[]): unknown[] {
+  const result = [];
+  for (let i = 0; i < list1.length; i++) {
+    result.push(list1[i], list2[i]);
+  }
+  return result;
+}
+
 function zipListTheFunctionalWay(list1: string[], list2: number[]): (string | number)[] {
   return list1.reduce<(string | number)[]>((acc, value, index) => {
     acc.push(value, list2[index]);
@@ -9,3 +17,4 @@ const list1 = ['a', 'b', 'c'];
 const list2 = [1, 2, 3];
 
 console.log(zipListTheFunctionalWay(list1, list2));
+console.log(zipList(list1, list2));
